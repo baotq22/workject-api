f4b136ab164a5b624f57f713edaf9716f12adec7
baotq22 <87086420+baotq22@users.noreply.github.com>
2024-10-15 12:17:34 +0700

    Fix cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }))
