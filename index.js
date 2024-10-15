24ea6fec068e6809e6a67d3393efdd4da7ec4981
baotq22 <87086420+baotq22@users.noreply.github.com>
2024-10-15 12:19:18 +0700

    Fix cors and route

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
-app.use("/", router);
+app.use("/api", router);
 
 app.use(RouteNotFound);
 app.use(errorHandler);
